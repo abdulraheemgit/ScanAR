da6972b544549b73ca11e47887fb09c2f29c7dde
AR <AR>
2016-05-17 20:09:06 +0530

    commit 4

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 "# ScanAR"
 
 ScanAR is a security framwork for web application development. This will support both client side and server side security implementation as nodejs can support both.
+
 features
-    -- XSS detection engine
-    -- SQL injection detection engine
-    -- Input sanitization
-    -- HTML escaping funtionality
+    -XSS detection engine
+    -SQL injection detection engine
+    -Input sanitization
+    -HTML escaping funtionality
 
 usage
   npm install scan-a-r
+
+  after installing the package add the below code within scripts in your package.json
+
+  "prestart": "browserify "
+
+  ![Alt text](/relative/path/to/img.jpg? raw=true "Optional Title")
---
+++ src/preInstall.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+if (!fs.existsSync('..../scripts')){
+    fs.mkdirSync('..../scripts');
+}
+fs.closeSync(fs.openSync("..../scripts/browser.js", 'w'));
+
+
+var j = {};
+fs.readFile('..../package.json', 'utf8', function(err, data){
+  j = JSON.parse(data);
+  j.scripts.prestart = 'browserify ./node_modules/scan-a-r/src/index.js -o ./script/browser.js';
+  console.log(j);
+  fs.writeFile('..../package.json',JSON.stringify(j, null, 4), function(err){
+    if(err){
+      console.log('err');
+    }
+    console.log('all ok');
+  })
+});
+
+// var browserify = require('browserify');
+// var b = browserify();
+// b.add('./index.js');
+// b.bundle().pipe(process.stdout);
